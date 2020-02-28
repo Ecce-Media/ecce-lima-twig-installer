655e975ea55236fb6a957ca39d934e2c0b74b98c
Liam Mills <liam@ecce.uk>
2020-02-28 13:59:50 +0000

    remove folder stuffs, up version number

--- src/Installer.php
+++ src/Installer.php
@@ -12,16 +12,8 @@ class Installer extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        /* $prefix = substr($package->getPrettyName(), 0, 23);
-        if ('phpdocumentor/template-' !== $prefix) {
-            throw new \InvalidArgumentException(
-                'Unable to install template, phpdocumentor templates '
-                .'should always start their package name with '
-                .'"phpdocumentor/template-"'
-            );
-        } */
-
-        return 'templates/'.substr($package->getPrettyName(), 23);
+        // return 'templates/' . $package->getPrettyName();
+        return 'templates/';
     }
 
     /**
